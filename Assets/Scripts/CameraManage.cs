4ef7253eea21e185b4995fcdc6e6a05b45352615
Adrien BOSSI <bossi.adrien@gmail.com>
2018-02-01 20:28:26 +0100

    init project

---
+++ Assets/Scripts/CameraManage.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManage : MonoBehaviour {
+
+	public List<Vector3>			positions;
+	public List<Vector3>			rotations;
+
+	public List<ParticleSystem>		menuPS;
+
+	[HideInInspector]public int		step;
+    private Camera					camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = transform.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + (positions[step] - transform.position) / 10;
+		transform.localEulerAngles = transform.localEulerAngles + (rotations[step] - transform.localEulerAngles) / 10;
+
+		if (Input.GetKeyDown("1"))
+			step = 0;
+		if (Input.GetKeyDown("2"))
+			step = 1;
+		if (Input.GetKeyDown("3"))
+			step = 2;
+
+		if (step == 0)
+		{
+	        RaycastHit hit;
+	        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+	        
+	        if (Physics.Raycast(ray, out hit)) {
+	            ParticleSystem objectHit = hit.transform.GetComponent<ParticleSystem>();
+	            if (objectHit && !objectHit.isPlaying)
+	            	objectHit.Play();
+	            for (int i = 0 ; i < menuPS.Count ; i++)
+	            {
+	            	if ((!objectHit || menuPS[i] != objectHit) && menuPS[i].isPlaying)
+	            	{
+	            		menuPS[i].Stop();
+	            		menuPS[i].Clear();
+	            	}
+	            }
+	        }
+		}
+	}
+}
---
+++ Assets/Scripts/moveManage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveManage : MonoBehaviour {
+
+	public List<Vector3>			positions;
+	public List<Vector3>			rotations;
+
+	public CameraManage				camManage;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int step = camManage.step;
+		transform.localPosition = transform.localPosition + (positions[step] - transform.localPosition) / 10;
+		transform.localEulerAngles = transform.localEulerAngles + (rotations[step] - transform.localEulerAngles) / 10;
+	}
+}
